d64d1c0e3fbbc16bdb6c2d28730fc83977728683
SI Xiaolong <sixiaolonggithub@gmail.com>
2026-05-13 04:30:15 +0800

    Initial Commit

---
+++ NetworkSelectorForMacOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkSelectorForMacOS
+//
+//  Created by 司晓龙 on 2026/5/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetworkSelectorForMacOS/NetworkSelectorForMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkSelectorForMacOSApp.swift
+//  NetworkSelectorForMacOS
+//
+//  Created by 司晓龙 on 2026/5/13.
+//
+
+import SwiftUI
+
+@main
+struct NetworkSelectorForMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
